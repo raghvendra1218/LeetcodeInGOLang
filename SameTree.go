19791b2ba392c739d1d8bc93bca8be4ffe215455
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2020-03-09 20:58:47 -0700

    Same Tree - modified the condition for recursive call last condition

--- SameTree.go
+++ SameTree.go
@@ -1,11 +1,5 @@
 package main
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
 //Approach 1: Recursive Solution, for each node we can check if it's left sub tree is equal to the right subtree
 func isSameTree(p *TreeNode, q *TreeNode) bool {
 	if p == nil && q == nil {
@@ -17,8 +11,5 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 	if p.Val != q.Val {
 		return false
 	}
-	if p.Val == q.Val {
-		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
-	}
-	return false
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
