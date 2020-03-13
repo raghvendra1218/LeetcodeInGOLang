e6db0d8657675fbc26687740d28931b0993d109d
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2020-03-12 21:02:40 -0700

    Add generic file for single Linked List structure in main package

---
+++ ListNode.go
@@ -0,0 +1,7 @@
+package main
+
+//generic structure for the List node for any Single Linked List
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
--- MiddleOfTheLinkedList.go
+++ MiddleOfTheLinkedList.go
@@ -1,10 +1,5 @@
 package main
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
 func middleNode(head *ListNode) *ListNode {
 	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
