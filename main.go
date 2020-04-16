a6f69d6b3a76d01b91ee58214b822414389a70c1
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2020-04-16 03:17:03 -0700

    Add Test for Number of Island from main file

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	s := "(*)"
-	fmt.Print("Output: ", checkValidString(s))
+	fmt.Println("is valid string: ", checkValidString(s))
+	//time.Sleep(1 * time.Second)
+	grid := [][]byte{{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}}
+	fmt.Println("Number of Islands: ", numIslands(grid))
 }
