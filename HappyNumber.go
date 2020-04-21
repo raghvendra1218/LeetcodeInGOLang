68248b67823b88b6c8852a907c5a82feb3a93b41
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2020-04-20 22:36:44 -0700

    Refactored Code, add the Stack generic file in utils folder

--- HappyNumber.go
+++ HappyNumber.go
@@ -1,32 +1,9 @@
 package main
 
-var exists = struct{}{}
-
-type set struct {
-	m map[int]struct{}
-}
-
-func NewSet() *set {
-	s := &set{}
-	s.m = make(map[int]struct{})
-	return s
-}
-
-func (s *set) Add(value int) {
-	s.m[value] = exists
-}
-
-func (s *set) Remove(value int) {
-	delete(s.m, value)
-}
-
-func (s *set) Contains(value int) bool {
-	_, c := s.m[value]
-	return c
-}
+import stack "LeetcodeInGOLang/utils"
 
 func isHappy(n int) bool {
-	set := NewSet()
+	set := stack.NewSet()
 	for {
 		if n == 1 {
 			return true
